dc90cfd2beb38efe54eb93f873dbb15bc1abc829
Preston Piercey <prestonpiercey@tamu.edu>
2026-02-19 18:11:56 -0600

    missed semicolon

--- test/test_monitor_smoke.cpp
+++ test/test_monitor_smoke.cpp
@@ -23,7 +23,7 @@ int main(int argc, char** argv) {
       std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
-    std::this_thread::sleep_for(std::chrono::seconds(2))
+    std::this_thread::sleep_for(std::chrono::seconds(2));
 
     world.barrier();
     if (world.rank0()) {
