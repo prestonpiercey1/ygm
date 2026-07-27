4d457684aa0ae8d6aca93733d394ce8d71eec690
prestonpiercey1 <prestonpiercey@gmail.com>
2026-07-27 11:15:25 -0700

    First attempt at consolidating shm messages

--- include/ygm/detail/stats_shm_signal.hpp
+++ include/ygm/detail/stats_shm_signal.hpp
@@ -63,20 +63,11 @@ inline struct sigaction old_actions[num_tracked_signals];
 
 inline void chained_unlink_handler(int sig, siginfo_t* info,
                                    void* ucontext) {
-  constexpr char prefix_msg[] = "Caught signal ";
-  constexpr char suffix_msg[] =
-      " in chained handler. Initiating unlink for ygm shm segments.\n";
-  char signum[2] = {static_cast<char>(sig / 10 % 10 + '0'),
-                    static_cast<char>(sig % 10 + '0')};
 
-  // sizeof(msg)-1 for null term strings. Keeps byte count synced with msg length
-  // (void)! cast is warning supression; return not material if already in failure mode
-  (void)!write(STDOUT_FILENO, prefix_msg, sizeof(prefix_msg) - 1);
-  (void)!write(STDOUT_FILENO, signum, 2);
-  (void)!write(STDOUT_FILENO, suffix_msg, sizeof(suffix_msg) - 1);
-
+  // Unlink segments that have been opened with paths from live_comm_uuids
   // Create substrate to copy path_ids onto
   char shm_path[64];
+  char rank[2];
 
   for (const std::string& path_id : ygm::detail::live_comm_uuids) {
     // Construct Desired String Format: <shm_prefix><path_id+"\0"><remaining_endspace>
@@ -85,6 +76,23 @@ inline void chained_unlink_handler(int sig, siginfo_t* info,
     strncpy(shm_path + (sizeof(shm_prefix) - 1), path_id.c_str(), path_id.size() + 1);
 
     shm_unlink(shm_path);
+
+    rank[0] = path_id[5];
+    rank[1] = path_id[6];
+  }
+
+  // Inform user of unlink only once from rank 0.
+  if (rank[0] == '0' && rank[1] == '\0') {
+    constexpr char prefix_msg[] = "Caught signal ";
+    constexpr char suffix_msg[] = " in chained handler. Unlinked ygm shm segments.\n";
+    char signum[2] = {static_cast<char>(sig / 10 % 10 + '0'),
+                      static_cast<char>(sig % 10 + '0')};
+
+    // sizeof(msg)-1 for null term strings. Keeps byte count synced with msg length
+    // (void)! cast is warning supression; return not material if already in failure mode
+    (void)!write(STDOUT_FILENO, prefix_msg, sizeof(prefix_msg) - 1);
+    (void)!write(STDOUT_FILENO, signum, 2);
+    (void)!write(STDOUT_FILENO, suffix_msg, sizeof(suffix_msg) - 1);
   }
 
   // Forward to the previously installed handler.
